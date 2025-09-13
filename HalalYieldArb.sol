d181392afae598b189fcec0a50bf31cc70750bb4
mshkhan32 <mshkhan32@gmail.com>
2025-09-14 05:59:38 +0600

    Add HalalYieldArb starter Solidity contract

---
+++ HalalYieldArb.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HalalYieldArb {
+    // Sample starter contract
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+}
