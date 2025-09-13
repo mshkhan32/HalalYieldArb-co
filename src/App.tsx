c899c9bbbbaacd64d654c85ea407fa6bbc8c2552
mshkhan32 <mshkhan32@gmail.com>
2025-09-14 05:59:58 +0600

    Add frontend React starter App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>HalalYieldArb Frontend</h1>
+      <p>Welcome to the HalalYieldArb React App!</p>
+    </div>
+  );
+}
+
+export default App;
